580cc83137463822946df4644e6be2e1d878545c
okpalaChidiebere <okpalacollins4@gmail.com>
2021-06-06 19:42:00 -0600

    Add Sharing (Implicit intent)
    Here we dont know or care how our request will be fulfilled. Eg Call a number
    
    There are other types of implicit intent like
    - Picking contacts from the  contacts content provider (content://contacts/people)
    - Dailing a number
    - Send an Email (https://www.codegrepper.com/code-examples/javascript/react+native+open+gmail+app)
    
    Useful links
    https://stackoverflow.com/questions/43214062/open-maps-google-maps-in-react-native
    
    We dont really need to know which application that will fulfil this request and the user may not have the application installed in their app.

--- res/components/ForecastDetails.js
+++ res/components/ForecastDetails.js
@@ -1,15 +1,87 @@
 import React from "react"
-import { StyleSheet, Button, Text, View }  from "react-native"
+import { StyleSheet, Button, Text, View, Platform, Linking, Share, Alert }  from "react-native"
 import { primary_text } from "../values/colors"
 
 export default function ForecastDetails({ route, navigation}){
+
+    const openLocationInMap = async () => {
+        const addressString = "1600 Ampitheatre Parkway, CA"
+        const scheme = Platform.select({ ios: 'maps:0,0?q=', android: 'geo:0,0?q=' })
+        const geoLocartionUrl = scheme + addressString
+
+        const supported = await Linking.canOpenURL(geoLocartionUrl)
+
+        if (supported) {
+            await Linking.openURL(geoLocartionUrl)
+        }else{
+            //The user may not have any app installed that will fulfil our request
+            console.log("Couldn't call " + geoLocartionUrl
+            + ", no receiving apps installed!")
+        }
+    }
+
+    const openWebsite = async () => {
+        const url = "https://google.com"
+
+        const supported = await Linking.canOpenURL(url)
+
+        if (supported) {
+        // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+        // by some browser in the mobile
+        await Linking.openURL(url)
+        } else {
+            Alert.alert(`Don't know how to open this URL: ${url}`)
+        }
+    }
+
+    const handleOnClickShareTextButton = async () => {
+        
+        const title = "Learning How to Share"
+        const textThatYouWantToShare =
+            "Sharing the coolest thing I've learned so far. You should " +
+                "check out Udacity and Google's Android Nanodegree!"
+
+        /*
+        More content config options here
+        https://reactnative.dev/docs/share 
+        https://github.com/react-native-share/react-native-share/issues/866
+        https://github.com/react-native-share/react-native-share/issues/767
+        */
+        const content = { title, message: textThatYouWantToShare }
+
+        try {
+            const result = await Share.share(content)
+              
+            // The content was successfully shared.
+            if (result.action === Share.sharedAction) {
+                if (result.activityType) {
+                    // shared with activity type of result.activityType
+                } else {
+                    // shared
+                }
+            } else if (result.action === Share.dismissedAction) {
+                // The dialog has been dismissed.
+            }
+        }catch (error) {
+            console.log(error.message)
+        }
+    }
 
     const { weatherForDay } = route.params
 
     return (
         <View style={styles.container}>
-          <Text style={styles.display_weather}>{weatherForDay}</Text>
-          <Button title="Go back" onPress={() => navigation.goBack()} />
+            <Text style={styles.display_weather}>{weatherForDay}</Text>
+            <Button title="Go back" onPress={() => navigation.goBack()} />
+            <View style={{ marginTop: 50 }}>
+                <Button title="OPEN LOCATION IN MAP" onPress={() => openLocationInMap()} />
+            </View>
+            <View style={{ marginTop: 50 }}>
+                <Button title="OPEN WEBSITE" onPress={openWebsite} />
+            </View>
+            <View style={{ marginTop: 50 }}>
+                <Button title="SHARE TEXT CONTENT" onPress={handleOnClickShareTextButton} />
+            </View>
         </View>
     );
 }
